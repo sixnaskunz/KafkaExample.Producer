3b38e2dfa6e1a5f45603d3c0207b6e480742529d
Warune Pondsuraparb <warune.pondsuraparb@krungsri.com>
2024-02-15 16:04:36 +0700

    Add document and remove try catch

--- KafkaExample.Producer/Services/ProducerService.cs
+++ KafkaExample.Producer/Services/ProducerService.cs
@@ -1,35 +1,40 @@
 namespace KafkaExample.Producer.Services;
 
+/// <summary>
+/// Represents a service for producing messages to Kafka.
+/// </summary>
 public class ProducerService
 {
     private readonly ProducerConfig _config;
     private readonly ILogger<ProducerService> _logger;
 
-    public ProducerService(IOptions<KafkaOptions> options, ILogger<ProducerService> logger)
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ProducerService"/> class.
+    /// </summary>
+    /// <param name="options">The Kafka options.</param>
+    /// <param name="logger">The logger.</param>
+    public ProducerService(ILogger<ProducerService> logger, IOptions<KafkaOptions> options)
     {
+        _logger = logger;
         _config = new ProducerConfig
         {
             BootstrapServers = options.Value.BootstrapServers
         };
-        _logger = logger;
     }
+
+    /// <summary>
+    /// Sends a message asynchronously to the specified Kafka topic.
+    /// </summary>
+    /// <param name="request">The request containing the topic and message.</param>
+    /// <returns>The delivery result of the message.</returns>
     public async Task<DeliveryResult<Null, string>?> SendMessageAsync(RequestKafka request)
     {
-        DeliveryResult<Null, string>? deliveryResult = null;
-
         // If serializers are not specified, default serializers from
         // `Confluent.Kafka.Serializers` will be automatically used where
         // available. Note: by default strings are encoded as UTF8.
         using var producer = new ProducerBuilder<Null, string>(_config).Build();
-        try
-        {
-            deliveryResult = await producer.ProduceAsync(request.Topic, new Message<Null, string> { Value = request.Message });
-            _logger.LogInformation("Delivered '{Value}' to '{TopicPartitionOffset}'", deliveryResult.Value, deliveryResult.TopicPartitionOffset);
-        }
-        catch (ProduceException<Null, string> e)
-        {
-            _logger.LogError("Delivery failed: {Reason}", e.Error.Reason);
-        }
+        DeliveryResult<Null, string> deliveryResult = await producer.ProduceAsync(request.Topic, new Message<Null, string> { Value = request.Message });
+        _logger.LogInformation("Delivered '{Value}' to '{TopicPartitionOffset}'", deliveryResult.Value, deliveryResult.TopicPartitionOffset);
         return deliveryResult;
     }
 }
